7b450e3c375af3419a636d7824a764bea5283a1b
bernardwang <bernard@bernard.wang>
2017-02-07 15:35:59 -0600

    highlight style test

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -3,3 +3,11 @@ $(document).ready(function(){
 		$('#js-navbar').toggleClass('is-open');
 	});
 });
+
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 700);
+});
